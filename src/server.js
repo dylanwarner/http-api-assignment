354f1cec02579fcfa50afd52c8ebb3b0d1656bc0
Dylan Warner <dcw8084@g.rit.edu>
2019-09-19 18:55:47 -0400

    Fixed XML

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const url = require('url');
-const query = require('querystring'); 
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  'GET': {
+  GET: {
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getCSS,
     '/success': responseHandler.success,
@@ -21,18 +21,17 @@ const urlStruct = {
 };
 
 const onRequest = (request, response) => {
-  
   const parsedUrl = url.parse(request.url);
 
   const params = query.parse(parsedUrl.query);
 
-  //grab the 'accept' headers (comma delimited) and split them into an array
+  // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
 
   if (urlStruct[request.method][parsedUrl.pathname]) {
-    urlStruct[request.method][parsedUrl.pathname](request, response, params, acceptedTypes);
+    urlStruct[request.method][parsedUrl.pathname](request, response, acceptedTypes, params);
   } else {
-    urlStruct[request.method].notFound(request, response, params, acceptedTypes);
+    urlStruct[request.method].notFound(request, response, acceptedTypes, params);
   }
 };
 
